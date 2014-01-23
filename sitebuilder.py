6543d1419a1ff5f839592e1a3fc732258a402513
Jacob Peck <peckj@upstate.edu>
2014-01-23 11:52:22 -0500

    Added several pages.

--- sitebuilder.py
+++ sitebuilder.py
@@ -46,7 +46,39 @@ def tag(tag):
 def page(path):
   page = pages.get_or_404(path)
   return html_minify(render_template('page.html', page=page))
-#@+node:peckj.20140121082121.6645: *3* route '/404.html'
+#@+node:peckj.20140123082920.4917: *3* top-level nav
+#@+node:peckj.20140123082920.4911: *4* route '/about/'
+@app.route("/about/")
+def about():
+  return html_minify(render_template('about.html'))
+#@+node:peckj.20140123082920.4912: *4* route '/archives/'
+@app.route("/archives/")
+def archives():
+  return html_minify(render_template('archives.html', pages=pages))
+#@+node:peckj.20140123082920.4916: *4* route '/code-dump/'
+@app.route('/code-dump/')
+def codedump():
+  return tag('code-dump')
+
+#@+node:peckj.20140123082920.4921: *4* route '/csmm/'
+@app.route('/csmm/')
+def csmm():
+  return tag('csmm')
+
+#@+node:peckj.20140123082920.4922: *4* route '/tags/'
+@app.route("/tags/")
+def tags():
+  tags = {}
+  for page in pages:
+    ts = page.meta.get('tags',None)
+    if ts is not None and len(ts) > 0:
+      for tag in ts:
+        val = tags.get(tag, 0)
+        val += 1
+        tags[tag] = val
+  return html_minify(render_template('tags.html', tags=tags))
+#@+node:peckj.20140123082920.4919: *3* etc
+#@+node:peckj.20140121082121.6645: *4* route '/404.html'
 @app.route('/404.html')
 def error():
   return html_minify(render_template('error.html', error=404))
@@ -54,11 +86,11 @@ def error():
 @freezer.register_generator
 def error_generator():
   return ['/404.html']
-#@+node:peckj.20140121082121.6646: *3* route '/favicon.ico'
+#@+node:peckj.20140121082121.6646: *4* route '/favicon.ico'
 @app.route('/favicon.ico')
 def favicon():
   return  send_from_directory(app.static_folder, 'assets/img/favicon.ico')
-#@+node:peckj.20140121082121.6647: *3* route '/robots.txt'
+#@+node:peckj.20140121082121.6647: *4* route '/robots.txt'
 @app.route('/robots.txt')
 def favicon():
   return  send_from_directory(app.static_folder, 'assets/robots.txt')
@@ -69,6 +101,6 @@ if __name__ == "__main__":
     freezer.freeze()
     shutil.rmtree('build/static/.webassets-cache')
   else:
-    app.run(port=8000)
+    app.run(port=3000)
 
 #@-leo
